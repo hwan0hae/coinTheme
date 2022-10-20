517046bc24789c51c8fae5407a3be89b87e1f41c
김대환 <gimdaehwan@gimdaehwan-ui-MacBookAir.local>
2022-10-20 22:41:43 +0900

    메인페이지 url 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -13,7 +13,7 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Coins />} />
+        <Route path="/coinTheme" element={<Coins />} />
         <Route path="/:coinId" element={<Coin />}>
           <Route path="chart" element={<Chart />} />
           <Route path="price" element={<Price />} />
